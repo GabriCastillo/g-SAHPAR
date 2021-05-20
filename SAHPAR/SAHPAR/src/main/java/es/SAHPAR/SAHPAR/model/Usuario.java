1e640f936f7b4f3201a4c43de6185d28559f18a4
GabriCastillo <72267459+GabriCastillo@users.noreply.github.com>
2021-05-20 21:45:01 +0200

    Librerias de seguridad para el login

---
+++ SAHPAR/SAHPAR/src/main/java/es/SAHPAR/SAHPAR/model/Usuario.java
@@ -0,0 +1,52 @@
+package es.SAHPAR.SAHPAR.model;
+
+public class Usuario {
+	private String username;
+	private String passw;
+	private String foto;
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	public Usuario() {}
+	public void editarFoto() {}
+	public void avisarMedico() {}
+	public void añadirPaciente(Paciente paciente) {}
+	public void buscarPaciente() {}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassw() {
+		return passw;
+	}
+	public void setPassw(String passw) {
+		this.passw = passw;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+}
