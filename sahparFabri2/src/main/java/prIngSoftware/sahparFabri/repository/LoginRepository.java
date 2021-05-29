24fe57f2e36e0211e911153f157afe253299e259
FabrizioCC <80286150+FabrizioCC@users.noreply.github.com>
2021-05-29 16:16:31 +0200

    Delete sahparFabri2 directory

--- sahparFabri2/src/main/java/prIngSoftware/sahparFabri/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package prIngSoftware.sahparFabri.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import prIngSoftware.sahparFabri.model.Usuario;
-
-public interface LoginRepository extends JpaRepository<Usuario, String>{
-	
-}
--- sahparFabri2/src/main/java/prIngSoftware/sahparFabri/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package prIngSoftware.sahparFabri.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import prIngSoftware.sahparFabri.model.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Integer>{
-	
-}
