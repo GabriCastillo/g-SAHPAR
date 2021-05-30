81da2006fe6319ee3c0878d0480ea2a737d734c2
RaulDoblas <rauldofe@uma.es>
2021-05-31 01:30:42 +0200

    Update Sensor.java

--- SAHPAR-modelos/src/main/java/es/SAHPAR/SAHPAR/model/Sensor.java
+++ SAHPAR-modelos/src/main/java/es/SAHPAR/SAHPAR/model/Sensor.java
@@ -12,10 +12,28 @@ public class Sensor {
 	private Integer id;
 	private double frecuencia;
 	private String habitacion;
+	private boolean activado;
 	@OneToOne (mappedBy ="sensor")
 	Paciente ps;
 	public Sensor() {
-
+		activado=true;
+		int prob;
+		while(activado) {
+			prob=(int) (Math.random()*101);
+			if(prob<=20&&prob>0) {
+				frecuencia=Math.random()*(40);
+			}else if(prob<=50&&prob>20){
+				frecuencia=Math.random()*(59-40+1)+40;
+			}else{
+				frecuencia=Math.random()*(100-60+1)+60;
+			}
+		}
+	}
+	public boolean isActivado() {
+		return activado;
+	}
+	public void setActivado(boolean activado) {
+		this.activado = activado;
 	}
 	public double getFrecuencia() {
 		return frecuencia;
